e53a7ec0ae5d81afeff495a1ae426b61412447df
xiaocao <853044263@qq.com>
2022-03-01 21:50:56 +0800

    配置引入svg

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
-import x from "icons/money.svg"
+import  x from 'icons/money.svg'
+import b from "icons/Chart.svg"
+import c from "icons/note.svg"
+console.log(c)
+console.log(b)
 console.log(x)
 const NavRep = styled.div`
     > ul{
@@ -10,8 +14,10 @@ const NavRep = styled.div`
       li{
         line-height: 24px;
         width: 33.333%;
-        text-align: center;
         padding: 16px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
       }
     }
 `
@@ -20,12 +26,21 @@ const Nav = ()=>{
         <NavRep>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money"/>
+                    </svg>
                     <Link to="/Tab">标签页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#note"/>
+                    </svg>
                     <Link to="/Note">笔记页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#Chart"/>
+                    </svg>
                     <Link to="/Statistics">统计页</Link>
                 </li>
             </ul>
