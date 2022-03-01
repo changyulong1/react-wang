9ef84f43275f959c61ffa38d3155ccdb73bc15ef
xiaocao <853044263@qq.com>
2022-03-01 17:05:35 +0800

    使用react-router-dom设置路由跳转

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
 import React from 'react';
-function App() {
-  return (
-    <div className="App">
-        <button>按钮</button>
-    </div>
-  );
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+}
+    from "react-router-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/Tab">标签页</Link>
+                            </li>
+                            <li>
+                                <Link to="/Note">笔记页</Link>
+                            </li>
+                            <li>
+                                <Link to="/Statistics">统计页</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route path="/" element={<Tab/>}/>
+                        <Route path="/Tab" element={<Tab/>}/>
+                        <Route path="/Note" element={<Note/>}/>
+                        <Route path="/Statistics" element={<Statistics/>}/>
+                        <Route path="/*" element={<NoPage/>}/>
+
+                    </Routes>
+                </div>
+            </Router>
+        );
+    }
+}
+
+function NoPage() {
+    return <div>页面不存在</div>;
+}
+
+function Tab() {
+    return <h2>标签页</h2>;
+}
+
+function Note() {
+    return <h2>笔记也</h2>;
+}
+
+function Statistics() {
+    return <h2>统计</h2>;
 }
 
 export default App;
