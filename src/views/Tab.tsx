5bad8e2c66eb2b063bc3c3ca9af0817a7fc9c2a4
xiaocao <853044263@qq.com>
2022-03-02 14:29:29 +0800

    收入支出ui完成

--- src/views/Tab.tsx
+++ src/views/Tab.tsx
@@ -3,55 +3,91 @@ import React from "react";
 import styled from "styled-components";
 
 const TagsSection = styled.section`
-    background: #FFFF;padding: 12px 16px;
+  background: #FFFF;
+  padding: 12px 16px;
+
   > ol {
-    margin:0 -12px;
-    >li{
-      display: inline-block;font-size: 14px;
-      padding: 4px 18px;background: #D9D9D9;
-      border-radius: 18px;margin: 10px 12px;
+    margin: 0 -12px;
+
+    > li {
+      display: inline-block;
+      font-size: 14px;
+      padding: 4px 18px;
+      background: #D9D9D9;
+      border-radius: 18px;
+      margin: 10px 12px;
     }
   }
-  >button{
-    background: none;border: none;padding: 2px 4px;
-    border-bottom: 1px solid #666666;color: #999999;
+
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #666666;
+    color: #999999;
     margin-top: 18px;
   }
-   
-`
+
+`;
 const NotesSection = styled.section`
-    padding: 0 16px;
-    font-size: 14px;
-    > label{
-      display: flex;
-      align-items: center;
-      >span{
-        margin-right: 16px;
-        white-space: nowrap;
-      }
-      >input{
-        background: none;
-        border: none;
-        height: 73px;
+  padding: 0 16px;
+  font-size: 14px;
+
+  > label {
+    display: flex;
+    align-items: center;
+
+    > span {
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+
+    > input {
+      background: none;
+      border: none;
+      height: 73px;
+      width: 100%;
+      outline: none;
+    }
+  }
+`;
+const CategorySection = styled.section`
+  > ul {
+    display: flex;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      background: #C4C4C4;
+      text-align: center;
+      padding: 18px 0;
+      position: relative;
+
+      &.selector::before {
+        content: "";
+        display: block;
         width: 100%;
-        outline: none;
+        height: 4px;
+        background: #333333;
+        position: absolute;
+        bottom: 0;
       }
     }
-`
-const CategorySection =styled.section`
-    
-`
+  }
+`;
+const NumberPadSection = styled.section``;
 
-const Tab = ()=>{
-    return(
+
+const Tab = () => {
+    return (
         <Layout>
             <TagsSection>
-            <ol>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-            </ol>
+                <ol>
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ol>
                 <button>新增标签</button>
             </TagsSection>
             <NotesSection>
@@ -61,6 +97,12 @@ const Tab = ()=>{
                 </label>
             </NotesSection>
             <CategorySection>
+                <ul>
+                    <li className="selector">支出</li>
+                    <li>收入</li>
+                </ul>
+            </CategorySection>
+            <NumberPadSection>
                 <div>100</div>
                 <button>1</button>
                 <button>2</button>
@@ -76,8 +118,8 @@ const Tab = ()=>{
                 <button>ok</button>
                 <button>0</button>
                 <button>.</button>
-            </CategorySection>
+            </NumberPadSection>
         </Layout>
-    )
-}
-export default Tab
+    );
+};
+export default Tab;
