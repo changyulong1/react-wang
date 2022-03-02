e62d3e3fc9574017fe05ec5e336f9dad5ab1b75c
xiaocao <853044263@qq.com>
2022-03-02 13:52:24 +0800

    TagSection样式

--- src/views/Tab.tsx
+++ src/views/Tab.tsx
@@ -1,10 +1,65 @@
 import Layout from "../components/Layout";
 import React from "react";
+import styled from "styled-components";
+
+const TagsSection = styled.section`
+    background: #FFFF;padding: 12px 16px;
+  > ol {
+    margin:0 -12px;
+    >li{
+      display: inline-block;font-size: 14px;
+      padding: 4px 18px;background: #D9D9D9;
+      border-radius: 18px;margin: 10px 12px;
+    }
+  }
+  >button{
+    background: none;border: none;padding: 2px 4px;
+    border-bottom: 1px solid #666666;color: #999999;
+    margin-top: 18px;
+  }
+   
+`
+const NotesSection = styled.section`
+`
+const CategorySection =styled.section`
+    
+`
 
 const Tab = ()=>{
     return(
         <Layout>
-            <h2>标签页</h2>
+            <TagsSection>
+            <ol>
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ol>
+                <button>新增标签</button>
+            </TagsSection>
+            <NotesSection>
+                <label>
+                    <span>备注</span>
+                    <input type="text"/>
+                </label>
+            </NotesSection>
+            <CategorySection>
+                <div>100</div>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>ok</button>
+                <button>0</button>
+                <button>.</button>
+            </CategorySection>
         </Layout>
     )
 }
