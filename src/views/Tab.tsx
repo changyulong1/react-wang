2534bf55e4e4036ea59158115d76088b350910e9
xiaocao <853044263@qq.com>
2022-03-02 14:04:58 +0800

    备注样式

--- src/views/Tab.tsx
+++ src/views/Tab.tsx
@@ -20,6 +20,23 @@ const TagsSection = styled.section`
    
 `
 const NotesSection = styled.section`
+    padding: 0 16px;
+    font-size: 14px;
+    > label{
+      display: flex;
+      align-items: center;
+      >span{
+        margin-right: 16px;
+        white-space: nowrap;
+      }
+      >input{
+        background: none;
+        border: none;
+        height: 73px;
+        width: 100%;
+        outline: none;
+      }
+    }
 `
 const CategorySection =styled.section`
     
@@ -40,7 +57,7 @@ const Tab = ()=>{
             <NotesSection>
                 <label>
                     <span>备注</span>
-                    <input type="text"/>
+                    <input type="text" placeholder="请填写备注"/>
                 </label>
             </NotesSection>
             <CategorySection>
